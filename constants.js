0f6b5ecf832a51b95d29c55a22bf320980bcf45b
Henry <henry.guo@dragonagile.com>
2021-09-18 17:06:04 -0400

    initial files, firebase working

---
+++ constants.js
@@ -0,0 +1,12 @@
+// Collections
+const USERS = 'users';
+const LOCATIONS = 'locations';
+const REVIEWS = 'reviews';
+const GIVEAWAYS = 'giveaways'
+
+export {
+    USERS,
+    LOCATIONS,
+    REVIEWS,
+    GIVEAWAYS,
+};
